f8b04bb5d50cfe0c43730be6002315f660b4c8bf
Milind Pandey (Captain Levi) <47604864+Pranshu2780@users.noreply.github.com>
2021-04-29 22:50:12 +0530

    Added Firebase and SCSS

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAF__3qlOFI-RPSu3QT4KzEotSIhAdAH-Y",
+    authDomain: "buygenix-17248.firebaseapp.com",
+    projectId: "buygenix-17248",
+    storageBucket: "buygenix-17248.appspot.com",
+    messagingSenderId: "699964343015",
+    appId: "1:699964343015:web:1144ec00ccdc0fa7b31842",
+    measurementId: "G-W15XC4ZJM8"
+  };
