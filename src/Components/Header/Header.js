2a46d1bb0203e8b828aeead6ae783120e4b07daa
Milind Pandey (Captain Levi) <47604864+Pranshu2780@users.noreply.github.com>
2021-04-30 23:33:21 +0530

    Edited Header and Added Logo

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,26 @@
-import React from 'react'
-import './Header.scss';
+import React from "react";
+import "./Header.scss";
 
 function Header() {
-    return (
-        <div className='header'>
-            
-        </div>
-    )
+  return (
+    <div className="header">
+      {/*logo in Header*/}
+      <img className="header_logo" src="../../public/logo.jfif" />
+      {/* Search Bar */}
+      <div className="search_bar">
+        <input className="search_bar_text" type="text" />
+      </div>
+
+      {/* Div containing Sign In, Order, options */}
+      <div className="header_right_container">
+        <div className="Sign_In"></div>
+
+        <div className="Orders"></div>
+
+        <div className="Prime"></div>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
