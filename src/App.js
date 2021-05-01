fec2370666a7419d5f92392c83db219c8b6e218a
Milind Pandey (Captain Levi) <47604864+Pranshu2780@users.noreply.github.com>
2021-05-01 23:00:33 +0530

    Added Home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.scss';
-import './Components/Header/Header';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="app">
         {/* // ** Header Component */}  
-        {/* <Header/> */}
+        <Header />
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,24 +1,34 @@
 import React from "react";
 import "./Header.scss";
+import Buygenix_logo from "./Buygenix_logo.png";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
 function Header() {
   return (
     <div className="header">
       {/*logo in Header*/}
-      <img className="header_logo" src="../../public/logo.jfif" />
+      <img className="header_logo" src={Buygenix_logo} />
       {/* Search Bar */}
       <div className="search_bar">
         <input className="search_bar_text" type="text" />
+        <SearchIcon className="search_icon" />
       </div>
 
       {/* Div containing Sign In, Order, options */}
       <div className="header_right_container">
-        <div className="Sign_In"></div>
+        <div className="sign_in">Sign In</div>
 
-        <div className="Orders"></div>
+        <div className="orders">Orders</div>
 
-        <div className="Prime"></div>
+        <div className="prime">Prime</div>
       </div>
+
+      <div className="basket">
+        <ShoppingCartIcon className="basket_icon" />
+        <span className="count">0</span>
+      </div>
+
     </div>
   );
 }
